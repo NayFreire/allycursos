7a28e65ef8293017396f969e3b176c1bcfa37153
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-05-19 20:03:46 -0300

    Usando useState para pegar as informações do formulário - ainda sem sucesso

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './images/allylogotransp.png'
-import InputData from './components/inputData'
+import {useState} from 'react'
 
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('')
+  const [startDate, setStartDate] = useState('')
+  const [endDate, setEndDate] = useState('')
+  const [duration, setDuration] = useState('')
+
   return (
     <div className="container">
       <div className="main">
@@ -11,24 +16,26 @@ function App() {
         <form className='info' action=''>
           <div className='divInput'>
             <label htmlFor="">Nome</label>
-            <input type="text" className='inputs' />
+            <input type="text" className='inputs' onChange={(e) => setName(e.target.value)}/>
           </div>
           <div className='divInput'>
             <label htmlFor="">Data de Início</label>
-            <InputData/>
+            <input type="date" name="dateStart" id="dateStart" className='inputs' onChange={(e) => setStartDate(e.target.value)}/>
           </div>
           <div className='divInput'>
             <label htmlFor="">Data de Fim</label>
-            <InputData/>
+            <input type="date" name="dateEnd" id="dateEnd" className='inputs' onChange={(e) => setEndDate(e.target.value)}/>
           </div>
           <div className='divInput'>
             <label htmlFor="">Duração (Semanas)</label>
-            <input type="text" className='inputs' />
+            <input type="text" className='inputs' onChange={(e) => {setDuration(e.target.value)}}/>
           </div>
-          <button type="submit">Cadastrar</button>
+          <button type="submit" onClick={() => {
+            console.log('nome: ', name)
+          }}>Cadastrar</button>
         </form>
       
-        <div className="list">
+        {/* <div className="list">
           <label htmlFor="">Lista de cursos</label>
           <table>
             <tr>
@@ -45,7 +52,7 @@ function App() {
               <td>1</td>
             </tr>
           </table>
-        </div>
+        </div> */}
       </div>
     </div>
   );
